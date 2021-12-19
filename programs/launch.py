505b0e35b5a0fe7f0c223d4a67976a30a01811bd
John Eagle <john.p.eagle@gmail.com>
2021-12-19 01:27:40 -0500

    changed audio to 16 bit

--- programs/launch.py
+++ programs/launch.py
@@ -1,7 +1,7 @@
 import pygame
 
 sfName = "../apollo11launch.wav"
-pygame.mixer.init()
+pygame.mixer.init(frequency=48000)
 pygame.mixer.music.load(sfName)
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
