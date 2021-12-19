7ff06a3b2a93b0e67be9a2517fcc205328f71b1c
John Eagle <john.p.eagle@gmail.com>
2021-12-19 17:00:39 -0500

    combining launch and lights

--- programs/launch.py
+++ programs/launch.py
@@ -1,8 +1,32 @@
-import pygame, threading
+import pygame, threading, os
+
+def startUp():
+    returnedValue = serverCall = "sudo ../../fadecandy/bin/fcserver-rpi saturnVconfig.json"
+    # call program here
+    os.system(serverCall)
+    print('returned value:', returnedValue)
+
+x = threading.Thread(target=startUp)
+x.start()
+
+time.sleep(2)
+
+
+print("RUNNING LIGHTS")
+# Light each LED in sequence, and repeat.
+numLEDs = 50
+client = opc.Client('localhost:7890')
 
 sfName = "../apollo11launch.wav"
 pygame.mixer.init(frequency=48000)
 pygame.mixer.music.load(sfName)
 pygame.mixer.music.play()
+
 while pygame.mixer.music.get_busy() == True:
+
+    for i in range(numLEDs):
+		pixels = [ (0,0,0) ] * numLEDs
+		pixels[i] = (255, 255, 255)
+		client.put_pixels(pixels)
+		time.sleep(0.01)
     continue
