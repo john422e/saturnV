8345fd5ef71f14e3412683035d2830a37ebb40b3
John Eagle <john.p.eagle@gmail.com>
2021-12-19 15:25:05 -0500

    added threading

--- programs/fcServer.py
+++ programs/fcServer.py
@@ -1,10 +1,14 @@
-import os
-import opc, time
+import os, time, threading
+import opc
 
-returnedValue = serverCall = "sudo ../../fadecandy/bin/fcserver-rpi saturnVconfig.json"
-# call program here
-os.system(serverCall)
-print('returned value:', returnedValue)
+def startUp():
+    returnedValue = serverCall = "sudo ../../fadecandy/bin/fcserver-rpi saturnVconfig.json"
+    # call program here
+    os.system(serverCall)
+    print('returned value:', returnedValue)
+
+x = threading.Thread(target=startUp)
+x.start()
 
 time.sleep(2)
 
