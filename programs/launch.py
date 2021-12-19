18a93c49daeb2a8f8f1ca97534d4370554a8bcd4
John Eagle <john.p.eagle@gmail.com>
2021-12-19 18:57:52 -0500

    tabs

--- programs/launch.py
+++ programs/launch.py
@@ -25,8 +25,7 @@ pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     for i in range(numLEDs):
         pixels = [ (0,0,0) ] * numLEDs
-		pixels[i] = (255, 255, 255)
-		client.put_pixels(pixels)
-		time.sleep(0.01)
-
+        pixels[i] = (255, 255, 255)
+        client.put_pixels(pixels)
+        time.sleep(0.01)
     continue
