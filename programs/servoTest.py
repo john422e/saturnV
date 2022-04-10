51c5dba2a235967d73265bfeea809eae107cecc0
John Eagle <john.p.eagle@gmail.com>
2022-04-10 11:08:01 -0400

    tbing

--- programs/servoTest.py
+++ programs/servoTest.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(0.5) # Initialization
 
 try:
     while True:
