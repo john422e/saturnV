7c37dbe238d9eba707590ed95c8b61f65b59cd11
John Eagle <john.p.eagle@gmail.com>
2022-04-10 11:21:56 -0400

    tbing

--- programs/servoTest.py
+++ programs/servoTest.py
@@ -6,22 +6,32 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 pwm = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+
+
+def setAngle(angle):
+    duty = angle / 18 + 3
+    GPIO.output(servoPin, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(servoPin, False)
+    pwm.ChangeDutyCycle(duty)
+
+
 pwm.start(0) # Initialization
 
-pwm.ChangeDutyCycle(5) # left -90 deg position
-time.sleep(1)
-pwm.ChangeDutyCycle(0)
-time.sleep(1)
+setting = True
 
-pwm.ChangeDutyCycle(7.5) # neutral position
-time.sleep(1)
-pwm.ChangeDutyCycle(0)
-time.sleep(1)
-
-pwm.ChangeDutyCycle(10) # right +90 deg position
-time.sleep(1)
-pwm.ChangeDutyCycle(0)
-time.sleep(1)
+while setting:
+    angle = input("SET ANGLE ('q' to quit): ")
+    if angle == 'q':
+        setting = False
+        break
+    else:
+        try:
+            setAngle(int(angle))
+        except:
+            print("INPUT INTEGERS ONLY")
+            continue
 
 pwm.stop()
 GPIO.cleanup()
