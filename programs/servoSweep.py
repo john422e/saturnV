947f11ddf24506c47deac5c8f3c7d3f0715befd0
John Eagle <john.p.eagle@gmail.com>
2022-04-11 23:52:40 -0400

    t

--- programs/servoSweep.py
+++ programs/servoSweep.py
@@ -15,7 +15,10 @@ while True:
     for i in range(0, 360):
         servo.value = math.sin(math.radians(i))
         sleep(0.01)
+    """
     # reverse it?
+
     for i in range(360, 0, -1):
         servo.value = math.sin(math.radians(i))
         sleep(0.01)
+    """
